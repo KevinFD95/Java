bb6a298c114e920c368dc6476ec8c8868e0f279c
KevinFD95 <emkf_95@hotmail.com>
2024-02-27 02:48:21 +0100

    2024/02/27 - New content

---
+++ PrjToDoListProgramLocal/src/classes/TaskController.java
@@ -0,0 +1,11 @@
+package src.classes;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+
+public class TaskController {
+
+}
